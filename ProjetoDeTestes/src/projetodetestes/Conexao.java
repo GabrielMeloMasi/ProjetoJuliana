03ea9e8c2ad99a17bab13a51ccce67cc5f51ad24
GabrielMeloMasi <56736050+GabrielMeloMasi@users.noreply.github.com>
2020-07-10 18:01:22 -0300

    Documentação do codigo
    -Gabriel Masi

--- ProjetoDeTestes/src/projetodetestes/Conexao.java
+++ ProjetoDeTestes/src/projetodetestes/Conexao.java
@@ -4,10 +4,17 @@ package projetodetestes;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+  /**
+  * Classe de conexão com o banco de dados
+  */
 public class Conexao {
-    
-    public static Connection faz_conexao() throws SQLException{
+    /**
+     * Método para conexão com banco 
+     */
+    public static Connection faz_conexao() throws SQLException{ 
+     /**
+     * Conexão com o banco ded dados
+     */
         try{
     Class.forName("com.mysql.cj.jdbc.Driver");
 		
